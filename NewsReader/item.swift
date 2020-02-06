50880b86a3c64a31a9ba6144aa59315972aac50f
taitai9847 <taitai9847@gmail.com>
2020-02-07 01:41:34 +0900

    initial

---
+++ NewsReader/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  NewsReader
+//
+//  Created by 石川　太洋 on 2019/09/09.
+//  Copyright © 2019 石川　太洋. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var link = ""
+}
